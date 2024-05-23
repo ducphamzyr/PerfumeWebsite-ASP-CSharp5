e1454c4a61696fb2d4b2e9fb85ca6690c6228b43
ZyrM <tduck.contact@gmail.com>
2024-05-24 00:33:34 +0700

    Add project files.

---
+++ AspDotNet_MVC/Migrations/20240523162104_PerfumeProject.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AspDotNet_MVC.Migrations
+{
+    public partial class PerfumeProject : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
